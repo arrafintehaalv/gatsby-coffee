4407bd1a8fba077d00b75887cf991ee1ff0f491d
Arraf Alvy <arrafintehaalvy@iut-dhaka.edu>
2019-04-05 00:09:28 +0600

    go go

---
+++ src/components/Home/Contact.js
@@ -0,0 +1,56 @@
+import React from "react"
+import Title from "../Globals/Title"
+export default function Contact() {
+  return (
+    <section className="contact py-5">
+      <Title title="contact us" />
+      <div className="row">
+        <div className="col-10 col-sm-8 col-md-6 mx-auto">
+          <form>
+            {/* name */}
+            <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                className="form-control"
+                name="name"
+                id="name"
+                placeholder="abc"
+              />
+            </div>
+            {/* email */}
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="text"
+                className="form-control"
+                name="email"
+                id="email"
+                placeholder="abc@email"
+              />
+            </div>
+            {/* description */}
+            <div className="form-group">
+              <label htmlFor="description">Description</label>
+              <textarea
+                name="description"
+                id="description"
+                cols="30"
+                rows="5"
+                className="form-control"
+                placeholder="your description goes here..."
+              />
+            </div>
+            {/* submit */}
+            <button
+              type="submit"
+              className="btn btn-yellow btn-block text-capitalize mt-5"
+            >
+              submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </section>
+  )
+}
