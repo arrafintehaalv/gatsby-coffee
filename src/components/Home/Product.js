33d5b36e714256689e6143ba3e061d6dc1a7c606
Arraf Alvy <arrafintehaalvy@iut-dhaka.edu>
2019-05-05 21:31:46 +0600

    hj

--- src/components/Home/Product.js
+++ src/components/Home/Product.js
@@ -10,7 +10,7 @@ export default function Product({ product }) {
           <h6>{product.title}</h6>
           <h6>BDT{product.price}</h6>
           <button
-            className="btn btn-yellow mt-3 text-capitalize snipcart-add-item"
+            className="b btn btn-yellow mt-3 text-capitalize snipcart-add-item"
             data-item-id={product.id}
             data-item-name={product.title}
             data-item-price={product.price}
